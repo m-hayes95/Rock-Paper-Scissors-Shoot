e49acf73ee21e18614dca3ce0da226d2220e00c1
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-02-01 13:35:44 +0000

    Added ray cast with debugging to check movement
    Added ray cast with debugging to check movement. This is for test and will need more adjusting later.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool isTile;
+    //public bool isTile;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +15,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //raycast hit to check if player can move
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        Vector3 fwd = transform.TransformDirection(new Vector3(0, -1,1));
+        //use forward vector from a game object in front of Player. Then rotate the object to the correct orientaion.
 
         //draw a debug line for ray cast
         Debug.DrawRay(transform.position, fwd, Color.magenta);
-       
-       
+
+        //saving ray cast hit data
+        RaycastHit hit;
+
+        //raycast out, check if something is hit
+        if (Physics.Raycast(transform.position, fwd, out hit))
+        {
+            Debug.Log("hit the" + hit.collider.gameObject.name);
+        }
 
         //move up
         if (Input.GetKeyDown("w"))
