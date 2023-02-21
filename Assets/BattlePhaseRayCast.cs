2aaa573dd0151c621082438cbfa5cc4c78363df7
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-02-21 15:45:21 +0000

    added battle phase start function
    added battle phase start function on battle ray cast script.

--- Assets/BattlePhaseRayCast.cs
+++ Assets/BattlePhaseRayCast.cs
@@ -5,25 +5,43 @@ using UnityEngine;
 public class BattlePhaseRayCast : MonoBehaviour
 {
     private const string PRE_BATTLE_PHASE_TILE = "PreBattleTile";
-    [SerializeField] private GameObject player;
+    //Ref to player and battle podium game object
+    [SerializeField] private GameObject player, playerBattlePodium;
+    //Ref to check if battle phase has been entered yet
+    [SerializeField] private bool playerHasEnteredBP = false;
 
+   
     private void Update()
     {
         //Cast ray cast downwards at tiles
         Vector3 battlePhaseCheckRayCast = transform.TransformDirection(new Vector3(0, -1, 0));
 
+        //Raycast hit data
         RaycastHit battlePhaseCheckRCHit;
 
+        //Draw a debug line for ray cast 
         Debug.DrawRay(transform.position, battlePhaseCheckRayCast, Color.black);
 
+        //Debug to check tile being hit
         if (Physics.Raycast(transform.position, battlePhaseCheckRayCast, out battlePhaseCheckRCHit))
         {
             Debug.Log("Battle Phase Ray Cast Check hit" +battlePhaseCheckRCHit.collider.gameObject.tag);
         }
 
-        if (battlePhaseCheckRCHit.collider != null && battlePhaseCheckRCHit.collider.gameObject.CompareTag(PRE_BATTLE_PHASE_TILE))
+        //If ray cast hit collision is with tile with Pre battle tag, and has not entered battle
+        if (battlePhaseCheckRCHit.collider != null && 
+            battlePhaseCheckRCHit.collider.gameObject.CompareTag(PRE_BATTLE_PHASE_TILE) && 
+            playerHasEnteredBP == false)
         {
+            //Enter battle phase if condions are met
             Debug.Log("StartBattlePhase");
+            //Players postion transformed to player podium location w/ offset on Y axis
+            float playerHeight = 1.5f;
+            player.transform.position = playerBattlePodium.transform.position + Vector3.up * playerHeight;
+            //Rotate player to face enemy
+            player.transform.Rotate(0,90,0);
+            //Set Battle phase bool to true
+            playerHasEnteredBP = true;
         }
     }
 }
