effeb0ff6e8f21dab2cc0c89560f8692d8b9c137
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-02-27 17:43:33 +0000

    Fixed null ref error on podium script and colour of podium not changing
    Also cleaned up script, removing useless code. Changed script location for podium colour to the visual object in the hierarchy.

--- Assets/PodiumColour.cs
+++ Assets/PodiumColour.cs
@@ -4,51 +4,39 @@ using UnityEngine;
 
 public class PodiumColour : MonoBehaviour
 {
-    //ref to player for scritps
-    //[SerializeField] private CurrentTileCheckRayCast playerTileCheck;
-    public GameObject playerTileCheck;
+    [SerializeField] private Material rockMat, paperMat, scissorsMat, specialMat; // ref to materials for each tile
+    private Renderer r; // ref to game object renderer.
+    [SerializeField] private TileManager tileManager; // ref to tile manager script
 
-    [SerializeField] private Material rockMat, paperMat, scissorsMat, specialMat;
-    //[SerializeField] private bool rock, paper, scissors, special;
-    private Renderer r;
-    //ref to tile manager script
-    private TileManager tileManager;
 
     private void Start()
     {
-        r = GetComponent<Renderer>();
-       //new bool player = rockTile.GetComponent<CurrentTileCheckRayCast>();
-       
+        r = GetComponent<Renderer>(); // assign renderer
+   
     }
 
     private void Update()
     {
         //player on rock tile set to correct mat
-        if (tileManager.rock == true)
+        if (tileManager.rock == true) 
         {
-            //Set the tile to rock materail
-            Debug.Log("player on rock");
-            r.material = rockMat;
-        }
+            //Debug.Log("player on rock");
+            r.material = rockMat; //Set the tile to rock materail
 
-        //player on paper tile set to correct mat
-        if (tileManager.paper == true)
+        }
+        if (tileManager.paper == true) 
         {
-            Debug.Log("player on paper");
+            //Debug.Log("player on paper");
             r.material = paperMat;
         }
-    
-        //player on scissors tile set to correct mat
-        if (tileManager.scissors == true)
+        if (tileManager.scissors == true) 
         {
-            Debug.Log("player on scissors");
+            //Debug.Log("player on scissors");
             r.material = scissorsMat;
         }
-
-        //player on special tile set to correct mat
         if (tileManager.special == true)
         {
-            Debug.Log("player on special");
+            //Debug.Log("player on special");
             r.material = specialMat;
         }
 
--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -10,24 +10,40 @@ public class TileManager : MonoBehaviour
     {
         //set player on rock tile bool to true
         rock = true;
+        //set others to false
+        paper = false;
+        scissors = false;
+        special = false;
     }
 
     public void PlayerOnPaperTile()
     {
         //set player on rock tile bool to true
         paper = true;
+        //set others to false
+        rock = false;
+        scissors=false;
+        special = false;
     }
 
     public void PlayerOnScissorsTile()
     {
         //set player on rock tile bool to true
         scissors = true;
+        //set others to false
+        rock = false;
+        paper=false;
+        special=false;
     }
 
     public void PlayerOnSpecialTile()
     {
         //set player on rock tile bool to true
         special = true;
+        //set others to false
+        rock = false;
+        paper = false;
+        scissors = false;
     }
 
 
