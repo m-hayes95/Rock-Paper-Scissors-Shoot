481a47048e6b7d8576c996749cec64fe8955dda4
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-03-07 12:50:51 +0000

    Added enemy ai movement on statemachine to move up
    Rotated and changed enemy up ray cast to correct position.
    Need to add other directions.
    Need to add move to podium

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -6,8 +6,9 @@ public class EnemyAI : MonoBehaviour
 {
     // ref to phase manager for checkpoint / phase check
     [SerializeField] private PhaseManager phaseManager;
+    [SerializeField] private EnemyUpRayCast enemyUpRayCast;
     // check if enemy can move this turn
-    [SerializeField] private bool canMove1 = false, canMove2 = false, canMove3 = false, canMove4 = false, battlePhase = false;
+    [SerializeField] private bool canMovePhase1 = false, canMovePhase2 = false, canMovePhase3 = false, canMovePhase4 = false, isBattlePhase = false;
     // statemachine for enemy ai moves
     public enum EnemyAISM { phase1, phase2, phase3, phase4, battlePhase }
     public EnemyAISM enemyState;
@@ -18,45 +19,69 @@ public class EnemyAI : MonoBehaviour
         switch (enemyState)
         {
             case EnemyAISM.phase1:
-                // enemy make 1st move
-                canMove1 = true;
-
-                // change phase
+                
                 if (phaseManager.checkpoint2 == true)
                 {
-                    enemyState = EnemyAISM.phase2;
+                    // check if player has moved, if yes enemy can now make 1st move
+                    canMovePhase1 = true;
+                    if (canMovePhase1 == true)
+                    {
+                        enemyUpRayCast.EnemyMoveUp(); // enemy make 1st move
+                        canMovePhase1 = false;
+                    }
+                    enemyState = EnemyAISM.phase2; // change current phase
                 }
                 break;
 
             case EnemyAISM.phase2:
-                canMove2 = true;
-
+                
                 if (phaseManager.checkpoint3 == true)
                 {
+                    canMovePhase2 = true;
+                    if (canMovePhase2 == true)
+                    {
+                        enemyUpRayCast.EnemyMoveUp();
+                        canMovePhase2 = false;
+                    }
                     enemyState = EnemyAISM.phase3;
                 }
                 break;
 
             case EnemyAISM.phase3:
-                canMove3 = true;
+                
 
                 if (phaseManager.checkpoint4 == true)
                 {
+                    canMovePhase3 = true;
+                    if (canMovePhase3 == true)
+                    {
+                        enemyUpRayCast.EnemyMoveUp();
+                        canMovePhase3 = false;
+                    }
                     enemyState = EnemyAISM.phase4;
                 }
                 break;
 
             case EnemyAISM.phase4:
-                canMove4 = true;
-
+            
                 if (phaseManager.battlePhaseCheckPoint == true)
                 {
+                    canMovePhase4 = true;
+                    if (canMovePhase4 == true)
+                    {
+                        enemyUpRayCast.EnemyMoveUp();
+                        canMovePhase4 = false;
+                    }
                     enemyState = EnemyAISM.battlePhase;
                 }
                 break;
 
             case EnemyAISM.battlePhase:
-                battlePhase = true;
+                isBattlePhase = true;
+                if (isBattlePhase == true)
+                {
+                    // move to enemy podium
+                }
                 break;
 
             default:
