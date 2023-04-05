7fbf2fa5f7c1daa86676b68238062f0babcd2fc5
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-04-05 12:34:56 +0100

    Updated comments

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    // Singleton called in game manager script, to reduce health of player when they lose a round.
     public static PlayerHealth Instance { get; private set; }
     private int maxHealth = 3;
     public int health;
@@ -22,11 +23,11 @@ public class PlayerHealth : MonoBehaviour
     }
     private void Start()
     {
-        health = maxHealth;
+        health = maxHealth; // Set the players starting Hp to the max hp value.
     }
     public int TakePlayersHealthAfterLoss (int healthLost)
     {
-        health -= healthLost;
-        return health;
+        health -= healthLost; // Reduce health by an incominng int value.
+        return health; // Return the updated health value as an int value.
     }
 }
