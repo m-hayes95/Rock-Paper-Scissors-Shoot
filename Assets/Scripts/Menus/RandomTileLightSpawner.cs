f2d96900582e3fe0ff612636dea677545f1cdaf1
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-04-14 17:23:05 +0100

    Added player ui moves banner and updated comments
    + Comments added in random tile spawner.

--- Assets/Scripts/Menus/RandomTileLightSpawner.cs
+++ Assets/Scripts/Menus/RandomTileLightSpawner.cs
@@ -7,28 +7,35 @@ public class RandomTileLightSpawner : MonoBehaviour
 {
     private int gridLength = 10;
     private int gridWidth = 5;
-    private float lightSpawnThresehold = 0.5f;
+    private float lightSpawnThresehold = 0.3f;
+    private float destroyPreviousTileLightTimer = 2f;
     [SerializeField]
     private GameObject tileLight;
     private Vector3 startTilePosition = new Vector3 (-4f,0f,-2f);
-    //float timer = 0f;
-    float destroyPreviousTileLightTimer = 2f;
+    
 
 
     private void Update()
     {
+        // Loop through each tile in the grid.
         for (int x = 0; x < gridLength; x++)
         {
             for (int y = 0 ; y < gridWidth; y++)
             {
-                float hightOffset = 0.45f;
+                float hightOffset = 0.45f; // Add a offset for the tile hight.
+                // Location of next tile, starting from the start tile position.
                 Vector3 nextTilePosition = startTilePosition + new Vector3(x, hightOffset, y);
+                // Create a random value to spawn a light.
                 float randomLightSpawnChance = Random.Range(0, 1000f);
                 
-                Debug.Log("Random chance value " + randomLightSpawnChance);
+                //Debug.Log("Random chance value " + randomLightSpawnChance);
+
+                // Check if random value is below the defined threshold.
                 if (randomLightSpawnChance <= lightSpawnThresehold)
                 {
+                    // Create a new game object with a instantiated light at the next position.
                     GameObject newLight = Instantiate(tileLight, nextTilePosition, Quaternion.identity);
+                    // Destroy the new instantiated game obejct after a pre defined timer.
                     Destroy(newLight, destroyPreviousTileLightTimer);
                 }
                 
