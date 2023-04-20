523f8a318a1e139dfa8cf110ab23054f6a806cef
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-04-20 13:28:24 +0100

    Added new card navigation for pause menu

--- Assets/Scripts/UI Scripts/PauseMenu.cs
+++ Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -13,6 +13,8 @@ public class PauseMenu : MonoBehaviour
 
     [SerializeField]
     private GameObject pauseMenuFirstButton, howToPlayMenuFirstButton, optionsMenuFirstButton, quitMenuFirstButton;
+    [SerializeField]
+    private GameObject buttonCard1, buttonCard2, buttonCard3, buttonCard4, buttonCard5, buttonCard6, buttonCard7, buttonCard8, buttonCard9, buttonCard10;
 
     private void Awake()
     { 
@@ -74,6 +76,49 @@ public class PauseMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    // Choose first button on new card with gamepad.
+    public void GoToCard1()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard1);
+    }
+    public void GoToCard2()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard2);
+    }
+    public void GoToCard3()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard3);
+    }
+    public void GoToCard4()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard4);
+    }
+    public void GoToCard5()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard5);
+    }
+    public void GoToCard6()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard6);
+    }
+    public void GoToCard7()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard7);
+    }
+    public void GoToCard8()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard8);
+    }
+    public void GoToCard9()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard9);
+    }
+    public void GoToCard10()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard10);
+    }
+
     private void OnEnable()
     {
         playerInputActions.Menu.Enable();
--- Assets/Scripts/UI Scripts/StartUIManager.cs
+++ Assets/Scripts/UI Scripts/StartUIManager.cs
@@ -8,11 +8,14 @@ public class StartUIManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject mainMenuFirstButton, howToPlayMenuFirstButton, optionsMenuFirstButton, tutorialMenuFirstButton;
+    [SerializeField] 
+    private GameObject buttonCard1, buttonCard2, buttonCard3, buttonCard4, buttonCard5, buttonCard6, buttonCard7, buttonCard8, buttonCard9, buttonCard10;
 
     private void Start()
     {
         SelectMainMenuFirstButton();
     }
+    
     public void StartGame()
     {
         // Start the game.
@@ -43,5 +46,47 @@ public class StartUIManager : MonoBehaviour
     public void SelectTutorialMenuFirstButton()
     {
         EventSystem.current.SetSelectedGameObject(tutorialMenuFirstButton);
+    }
+
+    // Choose first button on new card with gamepad.
+    public void GoToCard1() 
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard1);
+    }
+    public void GoToCard2()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard2);
+    }
+    public void GoToCard3()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard3);
+    }
+    public void GoToCard4()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard4);
+    }
+    public void GoToCard5()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard5);
+    }
+    public void GoToCard6()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard6);
+    }
+    public void GoToCard7()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard7);
+    }
+    public void GoToCard8()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard8);
+    }
+    public void GoToCard9()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard9);
+    }
+    public void GoToCard10()
+    {
+        EventSystem.current.SetSelectedGameObject(buttonCard10);
     }
 }
